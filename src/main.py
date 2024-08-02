81396bde6d31ba5bcbc8a5bdf32988181f980a9b
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-08-02 18:02:58 +0300

    removed argparse module

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 from dnserver import DNSServer
 from time import sleep
-from argparse import ArgumentParser
+from sys import argv
 
+PRIMARY_SERVER = "208.67.222.222"
+SECONDARY_SERVER = "208.67.220.220"
 
 
 def main(): 
     
-    args = parse_args()
-    
-    server = DNSServer(upstream=args.secondary)
+    server = DNSServer(upstream = (SECONDARY_SERVER if argv[-1] == "--secondary" else PRIMARY_SERVER))
     
     server.start()
     
@@ -22,12 +22,6 @@ def main():
         server.stop()
      
     server.stop()
-
-def parse_args():
-    parser = ArgumentParser("DNS Server", "dns [--secondry]", description="An upstream dns server")
-    parser.add_argument("--secondary", "-s", help="", nargs="?", default="208.67.222.222", const="208.67.220.220")
-        
-    return parser.parse_args()
 
 if __name__ == "__main__":
     main()
