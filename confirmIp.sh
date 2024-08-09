a1f9de33b96ef344f436c671edaaf3b453d93ef0
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-08-09 19:37:53 +0300

    autoremove after confirming ip

--- confirmIp.sh
+++ confirmIp.sh
@@ -6,16 +6,16 @@ cd ip && npm install && cd ..
 
 python3 ip/ip_confirm.py
 
-# sudo apt remove nodejs npm chromium-browser -y
+sudo apt remove nodejs npm chromium-browser -y
 
-# sudo apt autoremove -y
+sudo apt autoremove -y
 
-# sudo apt autoclean -y
+sudo apt autoclean -y
 
-# sudo apt clean -y
+sudo apt clean -y
 
-# sudo apt-get autoremove -y
+sudo apt-get autoremove -y
 
-# sudo apt-get autoclean -y
+sudo apt-get autoclean -y
 
-# sudo apt-get clean -y
+sudo apt-get clean -y
