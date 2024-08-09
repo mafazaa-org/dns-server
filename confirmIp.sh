b1138e291679fdb1eac9b518e0e5437de29226e0
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-08-09 17:32:47 +0300

    added confirmIp script

---
+++ confirmIp.sh
@@ -0,0 +1,19 @@
+sudo apt update
+
+sudo apt install chromium-browser nodejs npm -y
+
+python3 src/ip_confirm.py
+
+sudo apt remove nodejs npm chromium-browser -y
+
+sudo apt autoremove -y
+
+sudo apt autoclean -y
+
+sudo apt clean -y
+
+sudo apt-get autoremove -y
+
+sudo apt-get autoclean -y
+
+sudo apt-get clean -y
