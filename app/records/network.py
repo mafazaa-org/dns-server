9ad64b214af2a4c28a6686b21945e78a255b0190
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-03-09 11:02:41 +0200

    removed the print in network

--- app/records/network.py
+++ app/records/network.py
@@ -49,7 +49,6 @@ class Network(Record):
         handler: DNSHandler,
     ):
         server = PUBLIC_DNS if Record.DB.exists(host) else UPSTREAM
-        print(server)
         try:
             if handler.protocol == "udp":
                 proxy_r = request.send(
