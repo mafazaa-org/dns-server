a59cb5b4f8f1de0b35d194c1b5fdf9f155fff65b
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-03-16 07:37:08 +0200

    removed the print statement

--- app/records/network.py
+++ app/records/network.py
@@ -53,8 +53,7 @@ class Network(Record):
             PUBLIC_DNS.append(server)
         else:
             server = UPSTREAM
-            
-        print(server)
+        
         try:
             if handler.protocol == "udp":
                 proxy_r = request.send(
